a23d0fd96b1b636e66e62ffa1f1e15774e1292d9
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-21 10:33:49 +0530

    get data from data.js by spreding

--- demo-app/src/App.jsx
+++ demo-app/src/App.jsx
@@ -1,12 +1,22 @@
 import './index.css'
 import Header from './components/Header'
 import Card from './components/Card'
+import data from './data'
 function App() {
+
+  const card = (data.map(item =>{
+    return(
+      <Card
+        key={item.id}
+        {...item}
+      />
+    )
+  }))
 
   return (
     <div>
       <Header></Header>
-      <Card></Card>
+      {card}
     </div>
   )
 }
