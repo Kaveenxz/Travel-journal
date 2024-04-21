611ba67b46cf4e3545eca0b3a840dc3ca45f4d6a
Kaveenxz <kaveenhansithx@gmail.com>
2024-04-21 10:37:36 +0530

    finslize the project with getting data to card with props

--- demo-app/src/components/Card.jsx
+++ demo-app/src/components/Card.jsx
@@ -1,20 +1,20 @@
 import img from '/lake.avif'
 import location from '/location-icon.svg'
-function Card() {
+function Card(props) {
     return (
         <div className='card'>
             <div className="card--right">
-                <img src={img} alt="" className='card--image' />
+                <img src={props.imageUrl} alt="" className='card--image' />
             </div>
             <div className="card--left">
                 <div className="card--location">
                     <img src={location} alt="" />
-                    <span><h4>JAPAN</h4></span>
-                    <span><a href="">View on Google Maps</a></span>
+                    <span><h4>{props.location}</h4></span>
+                    <span><a href={props.googleMapsUrl}>View on Google Maps</a></span>
                 </div>
-                <h1 className='card--name'>Mount Fuji</h1>
-                <h4 className='card--date'>12 Jan, 2021 - 24 Jan, 2021</h4>
-                <p className='card--data'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h1 className='card--name'>{props.title}</h1>
+                <h4 className='card--date'>{props.startDate} - {props.endDate}</h4>
+                <p className='card--data'>{props.description}</p>
             </div>
         </div>
     )
